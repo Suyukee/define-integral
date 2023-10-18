675723995f62c297bbbf28c00b224bbbdd3e8f04
Suyukee <suyukee@ya.ru>
2023-10-18 14:12:18 +0300

    Чёт накидал

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 function App() {
   return (
     <div className="wrapper">
-      <h1>Калькулятор приближённого вычисления определённых интегралов</h1>
-      <div className="graphic">График</div>
+      <header>
+        <h3>КАЛЬКУЛЯТОР</h3>
+        <p>Приближённое вычисление определённых интегралов</p>
+      </header>
+
       <div className="content">
-        Поле поле поле
+        
+
+        <form>
+
+          <select id="method">
+            <option value={1}>Левых прямоугольников</option>
+            <option value={2}>Правых прямоугольников</option>
+          </select>
+
+          <div className="function">
+            <p>f(x) =</p>
+            <input defaultValue="Math.pow(x, 2)" />
+          </div>
+
+          <div className="limit">
+            <div className="limit-a">
+              <p>a =</p>
+              <input type="number" defaultValue="1" />
+            </div>
+
+            <div className="limit-b">
+              <p>b =</p>
+              <input defaultValue="10" />
+            </div>
+          </div>
+
+          <div className="number">
+            <p>n =</p>
+            <input defaultValue="10"/>
+          </div>
+
+          <div className="float">
+            <p>Знаков после запятой:</p>
+            <div className="slider">
+              <input type="range" min="0" max="10" defaultValue= "3"/>
+              <span className="number">3</span>
+            </div>
+          </div>
+
+        </form>
+
       </div>
+
     </div>
   );
 }
